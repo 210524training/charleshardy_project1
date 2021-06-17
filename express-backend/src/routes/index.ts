c6ccc2e255782760204d24c9b1f59c72f15a332a
charleshardy1 <charlesgoffhardy@gmail.com>
2021-06-17 15:56:54 -0700

    clean up base router

--- express-backend/src/routes/index.ts
+++ express-backend/src/routes/index.ts
@@ -6,22 +6,7 @@ import User from '../models/user';
 
 const baseRouter = Router();
 
-/* GET home page */
-baseRouter.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../public/views/index.html'));
-});
 
-/* GET login page */
-baseRouter.get('/login', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../public/views/login.html'));
-});
-
-baseRouter.get('/json', async (req, res) => {
-  console.log('Our callback was invoked!');
-  // res.json({ data: 'This is sending back JSON' });
-
-  throw new Error('Something went wrong!');
-});
 
 baseRouter.post('/login', async (req: express.Request<unknown, unknown, { username: string, password: string }, unknown, {}>, res) => {
   const { username, password } = req.body;
@@ -34,12 +19,10 @@ baseRouter.post('/login', async (req: express.Request<unknown, unknown, { userna
 export async function logout(req: express.Request, res: express.Response): Promise<void> {
   if(req.session.user) {
     const { username } = req.session.user;
-
     req.session.destroy(() => {
       console.log(`${username} logged out`);
     });
   }
-  // If they aren't logged in, we don't need to do anything
 
   res.status(202).send();
 }
@@ -47,6 +30,6 @@ export async function logout(req: express.Request, res: express.Response): Promi
 baseRouter.post('/logout', logout);
 
 baseRouter.use('/api/v1/users', userRouter);
-baseRouter.use('/api/v1/restaurants', reimbursementRouter);
+baseRouter.use('/api/v1/reimbursements', reimbursementRouter);
 
 export default baseRouter;
