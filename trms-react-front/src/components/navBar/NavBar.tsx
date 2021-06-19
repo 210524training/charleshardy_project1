37ea28a7fcfad125b4ec705ea6246c27576dfbf3
charleshardy1 <charlesgoffhardy@gmail.com>
2021-06-19 15:56:08 -0700

    add logout

--- trms-react-front/src/components/navBar/NavBar.tsx
+++ trms-react-front/src/components/navBar/NavBar.tsx
@@ -1,58 +1,66 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
-//import { useAppDispatch, useAppSelector } from '../../hooks';
-//import { logout, selectUser, UserState } from '../../slices/user.slice';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { logoutAsync, selectUser, UserState } from '../../slices/user.slice';
 
-type Props = {
-}
 
-/*
-const Navbar: React.FC<Props> = (props) => {
 
+
+const navbar: React.FC<unknown> = () => {
   const history = useHistory();
   const dispatch = useAppDispatch();
   // We "Select" the User data from the state
   const user = useAppSelector<UserState>(selectUser);
 
-  const handleLogout = () => {
-    dispatch(logout());
-
-    history.push('/');
+  const handleLogout = async() => {
+    const result = await dispatch(logoutAsync());
+    if(result.meta.requestStatus === 'fulfilled'){
+      history.push('/');
+    }else{
+      window.alert("logout failed!");
+    }
   }
-*/
-
-const navbar: React.FC<unknown> = (props) => {
-    return(
-      <>
-        <nav className="navbar navbar-expand-md navbar-light fixed-top bg-primary primary-color">
-          <div id="nav" className="container-fluid">
-            <NavLink className="navbar-brand text-light" to="/">GrubDash</NavLink>
-            <button className="navbar-toggler secondary-color-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-              <span className="navbar-toggler-icon secondary-color-2"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <NavLink className="nav-link text-light" to="/restaurants">Restaurants</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link text-light" to="/clicker">Clicker</NavLink>
-                </li>
-              </ul>
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item">
+  return(
+    <>
+      <nav className="navbar navbar-expand-md navbar-light fixed-top bg-primary primary-color">
+        <div id="nav" className="container-fluid">
+          <NavLink className="navbar-brand text-light" to="/">TRMS</NavLink>
+          <button className="navbar-toggler secondary-color-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span className="navbar-toggler-icon secondary-color-2"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link text-light" to="/restaurants">Restaurants</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link text-light" to="/clicker">Clicker</NavLink>
+              </li>
+            </ul>
+            <ul className="navbar-nav ms-auto">
+              { !user ? (
+                <>
+                  <li className="nav-item">
                     <NavLink className="nav-link text-light" to="/login">Login</NavLink>
-                </li>
-                <li className="nav-item">
+                  </li>
+                  <li className="nav-item">
                     <NavLink className="nav-link text-light" to="/register">Register</NavLink>
+                  </li>
+                </>
+                ) : (
+                <li className="nav-item nav-link text-light circle">
+                  <button type="button" className="btn btn-primary primary-color" onClick={handleLogout}>Logout</button>
                 </li>
-              </ul>
-            </div>
+                )
+              }
+            </ul>
           </div>
-        </nav>
-        <div className="spacer"></div>
-      </>
-    );
+        </div>
+      </nav>
+      <div className="spacer"></div>
+    </>
+  );
 }
 
 export default navbar;
