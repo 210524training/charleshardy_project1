dafdb95d8d8bf6c184b907f143919ebcde659481
charleshardy1 <charlesgoffhardy@gmail.com>
2021-06-20 17:41:14 -0700

    impl register and getsupervisor/deptHeads

--- trms-react-front/src/remote/TRMS-backend/TRMS.api.ts
+++ trms-react-front/src/remote/TRMS-backend/TRMS.api.ts
@@ -1,7 +1,55 @@
 import User from "../../models/user";
 import TrmsClient from "./TRMS.client";
 import httpCodes from 'http-status-codes';
+import trmsClient from "./TRMS.client";
+import user from "../../models/user";
 
+export const registerUser = async(user:User)=>{
+  const response = await TrmsClient.post<boolean>('api/v1/users',{
+    ...user,
+  });
+
+  return response.data as boolean;
+}
+export const getBenCos = async (): Promise<User[]>=>{
+  const response = await TrmsClient.post<User[]>('api/v1/users/role',{
+    value:"benefits coordinator"
+  });
+
+  return response.data as User[];
+}
+export const getsupervisors = async (department: string): Promise<User[]>=>{
+  const response = await TrmsClient.post<User[]>('api/v1/users/role',{
+    value:"supervisor"
+  });
+
+  const response2 = await TrmsClient.post<User[]>('api/v1/users/role',{
+    value:"department head"
+  });
+
+  const newSupervisors: User[]=[];
+
+  response.data.forEach((user:User)=>{
+    if(user.department === department) newSupervisors.push(user);
+  });
+
+  response2.data.forEach((user:User)=>{
+    if(user.department === department) newSupervisors.push(user);
+  });
+  return newSupervisors;
+}
+export const getDhs = async (department: string): Promise<User[]>=>{
+  const response = await TrmsClient.post<User[]>('api/v1/users/role',{
+    value:"department head"
+  });
+
+  const newDHS: User[]=[];
+
+  response.data.forEach((user:User)=>{
+    if(user.department === department) newDHS.push(user);
+  });
+  return newDHS;
+}
 export const sendLogin = async (username: string, password: string): Promise<User> => {
   const response = await TrmsClient.post<User>('/login', {
     username,
